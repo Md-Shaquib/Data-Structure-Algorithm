bc254890b7bdcaafa4b3b15115e6b74940772f03
Md Shaquib Ullah <76819128+Md-Shaquib@users.noreply.github.com>
2021-02-09 22:19:46 +0530

    Add files via upload

---
+++ 2_d_array.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*void spiral_array(int n, int m, int A[n][m])
+{
+    int row_start = 0;
+    int row_end = n-1;
+    int col_start = 0;
+    int col_end = m-1;
+
+    while(row_start<=row_end && col_start<= col_end)
+    {
+        //for rowstart
+        for(int col=col_start;col<=col_end;col++)
+        {
+            cout<<A[row_start][col]<<" ";
+        }
+        row_start++;
+
+        //for columnend
+        for(int row=row_start;row<=row_end;row++)
+        {
+            cout<<A[row][col_end]<<" ";
+        }
+        col_end--;
+
+        //for rowend
+        for(int col= col_end;col>=col_start;col--)
+        {
+            cout<<A[row_end][col]<<" ";
+        }
+        row_end--;
+        
+        //for columnstart
+        for(int row=row_end;row>=row_start;row--)
+        {
+            cout<<A[row][col_start]<<" ";
+        }
+        col_start++;
+    }
+}
+*/
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int A[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>A[i][j];
+        }
+    }
+    //spiral_array(n,m,A);
+    int row_start = 0;
+    int row_end = n-1;
+    int col_start = 0;
+    int col_end = m-1;
+
+    while(row_start<=row_end && col_start<= col_end)
+    {
+        //for rowstart
+        for(int col=col_start;col<=col_end;col++)
+        {
+            cout<<A[row_start][col]<<" ";
+        }
+        row_start++;
+
+        //for columnend
+        for(int row=row_start;row<=row_end;row++)
+        {
+            cout<<A[row][col_end]<<" ";
+        }
+        col_end--;
+
+        //for rowend
+        for(int col= col_end;col>=col_start;col--)
+        {
+            cout<<A[row_end][col]<<" ";
+        }
+        row_end--;
+        
+        //for columnstart
+        for(int row=row_end;row>=row_start;row--)
+        {
+            cout<<A[row][col_start]<<" ";
+        }
+        col_start++;
+    }
+    return 0;
+}
